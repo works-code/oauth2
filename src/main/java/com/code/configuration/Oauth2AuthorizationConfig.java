f9b16367d828d8499ecdef177cf28c11a6576dd7
works-code <im.yoolee@gmail.com>
2021-12-21 21:28:17 +0900

    first commit

--- src/main/java/com/code/configuration/Oauth2AuthorizationConfig.java
+++ src/main/java/com/code/configuration/Oauth2AuthorizationConfig.java
@@ -2,6 +2,7 @@ package com.code.configuration;
 
 import com.code.service.UserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authorization.AuthorizationManager;
@@ -10,6 +11,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.sql.DataSource;
 
 @EnableAuthorizationServer
 @Configuration
@@ -21,9 +27,14 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+        security.tokenKeyAccess("permitAll()") // 모두 허용하지 않으면 해당 서버에서 토큰 접근이 불가능 하여 토큰을 DB에서 찾을 수 없다.
+                .checkTokenAccess("isAuthenticated()") // 인증된 사용자만 토큰 체크 가능
+                .allowFormAuthenticationForClients();
     }
 
     // client 설정
@@ -40,10 +51,17 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
                 .autoApprove(true); // 권한 동의는 자동으로 yes (false 로 할시 권한 동의 여부를 묻는다.)
     }
 
+    // 토큰 DB 저장
+    @Bean
+    public TokenStore tokenStore(){
+        return new JdbcTokenStore(dataSource);
+    }
+
     // 인증, 토큰 설정
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager) // grant_type password를 사용하기 위함 (manager 지정 안할시 password type 으로 토큰 발행시 Unsupported grant type: password 오류 발생)
-                .userDetailsService(userDetailService); // refrash token 발행시 유저 정보 검사 하는데 사용하는 서비스 설정
+                .userDetailsService(userDetailService) // refrash token 발행시 유저 정보 검사 하는데 사용하는 서비스 설정
+                .tokenStore(tokenStore());
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,26 @@
-create table user(
+CREATE TABLE IF NOT EXISTS user(
     id varchar(20),
     password varchar(100),
     nickname varchar(100),
     state char(1)
+);
+
+-- access token 저장 > jwt 아닐때 유효 토큰 검증시 사용
+CREATE TABLE IF NOT EXISTS `oauth_access_token`
+(
+    `token_id`          VARCHAR(256) NULL,
+    `token`             BLOB         NULL,
+    `authentication_id` VARCHAR(256) NOT NULL,
+    `user_name`         VARCHAR(256) NULL,
+    `client_id`         VARCHAR(256) NULL,
+    `authentication`    BLOB         NULL,
+    `refresh_token`     VARCHAR(256) NULL,
+    PRIMARY KEY (`authentication_id`)
+);
+-- refresh token 저장 > jwt 아닐때 유효 토큰 검증시 사용
+CREATE TABLE IF NOT EXISTS `oauth_refresh_token`
+(
+    `token_id`       VARCHAR(256) NULL,
+    `token`          BLOB         NULL,
+    `authentication` BLOB         NULL
 );

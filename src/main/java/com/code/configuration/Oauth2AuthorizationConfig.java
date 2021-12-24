d42d826d49b6c0bb299adfe298194d73c5a90381
works-code <im.yoolee@gmail.com>
2021-12-24 21:49:03 +0900

    client 설정 메모리 > DB로 관리

--- src/main/java/com/code/configuration/Oauth2AuthorizationConfig.java
+++ src/main/java/com/code/configuration/Oauth2AuthorizationConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -40,15 +42,7 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     // client 설정
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory() // 클라이언트 정보는 메모리를 이용 한다.
-                .withClient("clientId") // 클라이언트 아이디
-                .secret("{noop}secretKey") // 시크릿키 ({} 안에 암호화 알고리즘을 명시 하면 된다. 암호화가 되어 있지 않다면 {noop}로 설정 해야 한다. 실제 요청은 암호화 방식인 {noop}를 입력 하지 않아도 된다.)
-                .authorizedGrantTypes("authorization_code","password", "refresh_token", "client_credentials") // 가능한 토큰 발행 타입
-                .scopes("read", "write") // 가능한 접근 범위
-                .accessTokenValiditySeconds(60) // 토큰 유효 시간 : 1분
-                .refreshTokenValiditySeconds(60*60) // 토큰 유효 시간 : 1시간
-                .redirectUris("http://localhost:8081/callback") // 가능한 redirect uri
-                .autoApprove(true); // 권한 동의는 자동으로 yes (false 로 할시 권한 동의 여부를 묻는다.)
+        clients.jdbc(dataSource);
     }
 
     // 토큰 DB 저장
@@ -57,11 +51,18 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
         return new JdbcTokenStore(dataSource);
     }
 
+    // 권한 동의 DB 저장
+    @Bean
+    public ApprovalStore approvalStore() {
+        return new JdbcApprovalStore(dataSource);
+    }
+
     // 인증, 토큰 설정
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager) // grant_type password를 사용하기 위함 (manager 지정 안할시 password type 으로 토큰 발행시 Unsupported grant type: password 오류 발생)
                 .userDetailsService(userDetailService) // refrash token 발행시 유저 정보 검사 하는데 사용하는 서비스 설정
-                .tokenStore(tokenStore());
+                .tokenStore(tokenStore())
+                .approvalStore(approvalStore());
     }
 }
